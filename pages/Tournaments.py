63dd8ff3fef6e63fb791e43cb90a412b59b4e5bc
ygpek <ikygrk@gmail.com>
2026-01-05 22:46:06 +0100

    feat: first introduction of tournaments

--- pages/Tournaments.py
+++ pages/Tournaments.py
@@ -1,9 +1,42 @@
+import streamlit as st
+
 from ui.refresh import refresh_data_button
+from ui.tournament_selector import tournament_selector
 from data_load.load_sheet import load_history
 
 VARIABLE = "TOURNAMENT_HISTORY_ID"
 
 
+def highlight_win(val):
+    if val < 4:
+        color = "green"
+    elif val >= 4:
+        color = "red"
+    else:
+        color = ""
+    return f"color: {color}"
+
+
 refresh_data_button()
 
 df = load_history(VARIABLE)
+
+
+# --- Game selector ---
+st.subheader("Game history")
+game_ids = sorted(df["tournament_id"].unique())
+selected_game = tournament_selector(game_ids, key="tournament_games")
+
+game_df = df[df["game_id"] == selected_game].sort_values(by="place", ascending=True, ignore_index=True)
+st.dataframe(
+    game_df.style.applymap(
+        highlight_win,
+        subset=["place"],
+    ).format(
+        {
+            "place": "{:.0f}",
+        }
+    ),
+    hide_index=True,
+    use_container_width=True,
+)
---
+++ ui/tournament_selector.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+
+def tournament_selector(game_ids, key="tournament_selector"):
+    if not game_ids:
+        return None
+
+    if key not in st.session_state:
+        st.session_state[key] = len(game_ids) - 1  # start at latest game
+
+    col1, col2, col3 = st.columns([1, 2, 1])
+
+    with col1:
+        st.session_state[key] = max(0, st.session_state[key])  # safety
+        st.button(
+            "⬅️",
+            key=f"{key}_prev",
+            disabled=st.session_state[key] == 0,
+            on_click=lambda: st.session_state.__setitem__(key, st.session_state[key] - 1),
+        )
+
+    with col3:
+        st.session_state[key] = min(len(game_ids) - 1, st.session_state[key])  # safety
+        st.button(
+            "➡️",
+            key=f"{key}_next",
+            disabled=st.session_state[key] == len(game_ids) - 1,
+            on_click=lambda: st.session_state.__setitem__(key, st.session_state[key] + 1),
+        )
+
+    with col2:
+        st.markdown(
+            f"### Tournament #{game_ids[st.session_state[key]]} " f"({st.session_state[key]+1}/{len(game_ids)})"
+        )
+
+    return game_ids[st.session_state[key]]
