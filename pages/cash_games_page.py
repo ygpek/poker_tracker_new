47da6590e9a777a681fd900c1caa9354b481df94
ygpek <ikygrk@gmail.com>
2026-01-05 21:58:18 +0100

    feat: improvement in cash games style

--- pages/cash_games_page.py
+++ pages/cash_games_page.py
@@ -3,6 +3,17 @@ from data_load.cash_games import load_cash_games
 from calcs.cash_stats import calculate_cash_summary
 from ui.game_selector import game_selector
 
+
+def highlight_win(val):
+    if val > 0:
+        color = "green"
+    elif val < 0:
+        color = "red"
+    else:
+        color = ""
+    return f"color: {color}"
+
+
 st.header("💵 Cash Games")
 
 df = load_cash_games()
@@ -23,7 +34,6 @@ st.dataframe(
         {
             "Total buy-ins": "{:.2f}",
             "Buy-ins per game": "{:.2f}",
-            "Kc won": "{:.2f}",
             "Kc won per game": "{:.2f}",
             "Win standard deviation": "{:.2f}",
         }
@@ -39,7 +49,10 @@ selected_game = game_selector(game_ids, key="cash_games")
 
 game_df = df[df["game_id"] == selected_game].sort_values(by="win", ascending=False, ignore_index=True)
 st.dataframe(
-    game_df,
+    game_df.style.applymap(
+        highlight_win,
+        subset=["win"],
+    ),
     hide_index=True,
     use_container_width=True,
 )
