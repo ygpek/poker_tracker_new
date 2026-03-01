f83047b26204bf096de511b9b8ab7c48b2bb62db
ygpek <ikygrk@gmail.com>
2026-03-01 10:54:32 +0100

    feat: rearranging of debts

--- rearrange_debts.py
+++ rearrange_debts.py
@@ -37,7 +37,7 @@ def main():
         win=lambda df: df["amount_to_receive"] - df["amount_to_send"],
     )
     calculated_debts = calculate_debts(total_debts)
-    print(payees_grouped)
+    print(debts_df)
     # upload_debts(calculated_debts, "Перерасчет долгов")
 
 
